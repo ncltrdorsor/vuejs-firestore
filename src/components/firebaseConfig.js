76d96d731c153230b3493c4652a225cae94f0c64
ncltrdorsor <serrano.nicolas.jose@gmail.com>
2018-02-17 14:23:02 +0100

    Removed config info

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -1,8 +1,3 @@
 export default {
-    apiKey: "AIzaSyBZhrVhPTuNbVtlKGrYe9c37qBWoEZ7FuA",
-    authDomain: "vuefs-intro.firebaseapp.com",
-    databaseURL: "https://vuefs-intro.firebaseio.com",
-    projectId: "vuefs-intro",
-    storageBucket: "vuefs-intro.appspot.com",
-    messagingSenderId: "660991966212"
+//  This is where your configuration goes
 }
